61d2690926e31c98fd80330f2610ff40010caa49
Nimble Capricorn <Nimble.capricorn@gmail.com>
2023-06-17 10:54:10 +0200

    added variation to ExerciseClass

--- ExerciseClass.py
+++ ExerciseClass.py
@@ -1,28 +1,38 @@
-class Exercise(MaxWeight, isNeurologicallyTaxing, isWholeBody , Bodyparts, OptimalLoadPercentage, isTechniqueFocused, Tempo):
+class Exercise(MaxWeight, isNeurologicallyTaxing, isWholeBody , Bodyparts, OptimalLoadPercentage, isTechniqueFocused, Tempo, Variation):
     
-    MaxWeight=MaxWeight
-    isNeurologicallyTaxing=isNeurologicallyTaxing
-    isWholeBody=isWholeBody
-    Bodyparts=Bodyparts
-    OptimalLoadPercentage=OptimalLoadPercentage
-    isTechniqueFocused=isTechniqueFocused
-    Tempo=Tempo
+    MaxWeight = MaxWeight
+    isNeurologicallyTaxing = isNeurologicallyTaxing
+    isWholeBody = isWholeBody
+    Bodyparts = Bodyparts
+    OptimalLoadPercentage = OptimalLoadPercentage
+    isTechniqueFocused = isTechniqueFocused
+    Tempo = Tempo
+    Variation = Variation
 
     def Tempo(self):
         return self.Tempo
+
     def isTechniqueFocused(self):
         return self.isTechniqueFocused
+
     def OptimalLoadingPercentage(self):
         return self.OptimalLoadPercentage
+
     def Bodyparts(self):
         return self.Bodyparts
+
     def isWholeBody(self):
         return self.isWholeBody
+
     def isNeurologicallyTaxing(self):
         return self.isNeurologicallyTaxing
+
     def MaxWeight(self):
         return self.MaxWeight
 
+    def Variation(self):
+        return self.Variation
+    
     pass
 
 
