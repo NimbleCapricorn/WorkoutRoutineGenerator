46e8d0025090e7b5bb38a91df24482cc21258209
Nimble Capricorn <Nimble.capricorn@gmail.com>
2023-06-17 10:28:59 +0200

    created createworkout and exerciseClass python files

---
+++ CreateWorkout.py
@@ -0,0 +1,5 @@
+import os
+import datetime
+import ExerciseClass
+
+
---
+++ ExerciseClass.py
@@ -0,0 +1,30 @@
+class Exercise(MaxWeight, isNeurologicallyTaxing, isWholeBody , Bodyparts, OptimalLoadPercentage, isTechniqueFocused, Tempo):
+    
+    MaxWeight=MaxWeight
+    isNeurologicallyTaxing=isNeurologicallyTaxing
+    isWholeBody=isWholeBody
+    Bodyparts=Bodyparts
+    OptimalLoadPercentage=OptimalLoadPercentage
+    isTechniqueFocused=isTechniqueFocused
+    Tempo=Tempo
+
+    def Tempo(self):
+        return self.Tempo
+    def isTechniqueFocused(self):
+        return self.isTechniqueFocused
+    def OptimalLoadingPercentage(self):
+        return self.OptimalLoadPercentage
+    def Bodyparts(self):
+        return self.Bodyparts
+    def isWholeBody(self):
+        return self.isWholeBody
+    def isNeurologicallyTaxing(self):
+        return self.isNeurologicallyTaxing
+    def MaxWeight(self):
+        return self.MaxWeight
+
+    pass
+
+
+
+
