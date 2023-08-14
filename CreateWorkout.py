dea8ded19618d2c0e5facefa7b58c81f1f884cd9
Nimble Capricorn <Nimble.capricorn@gmail.com>
2023-08-14 11:40:41 +0200

    changed intensity handling (again)

--- CreateWorkout.py
+++ CreateWorkout.py
@@ -37,14 +37,22 @@ ProgressionSystem = "DailyUndulating"
 ##
 #Section 1 subsection B): data loading
 ##
-#This way of handling the prilepin chart is very artificial (as in: not intuitive). Maybe there is a better way of doing it, but I don't know how
-with open("PrilepinChart.csv", "r") as PrilepinFile:
-    reader = csv.reader(PrilepinFile, delimiter=";")
-    
-    for row in reader:
-        PrilepinRowList.append(row)
-        print(PrilepinRowList)
-        
+#This way of handling the prilepin chart is very artificial (as in: not intuitive). 
+# with open("PrilepinChart.csv", "r") as PrilepinFile:
+#     reader = csv.reader(PrilepinFile, delimiter=";")    
+#     for row in reader:
+#         PrilepinRowList.append(row)
+#         print(PrilepinRowList)
+
+
+#The better way to do it is with a partial function and trendlines based on the data as two variable functions demonstrated with trendlines below
+MaxIntensity=Difficulty.IntensityFunction("Max", [-0.0405, 0.6583, -5.776, 104.87])
+HeavyPIntensity=Difficulty.IntensityFunction("Heavy+", [-0.0049, +0.1003, -3.411, 97.711])
+HeavyIntensity=Difficulty.IntensityFunction("Heavy", [-0.01, + 0.1733, -0.4844, 92.825])
+ModPIntensity=Difficulty.IntensityFunction("Mod+", [-0.0124, + 0.2095, - 0.2897, 82.753])
+ModIntensity=Difficulty.IntensityFunction("Mod", [-0.0131, 0.2206, -0.1739, 77.707])
+LightPIntensity=Difficulty.IntensityFunction("Light+",[-0.0124, 0.2095, -0.2897, 82.753])
+LightIntensity=Difficulty.IntensityFunction("Light",[-0.0143, 0.2387, -0.0766, 72.67])
 
 
 with open("Exercises.csv", "r") as ExercisesFile:
