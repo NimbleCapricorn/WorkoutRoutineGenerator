05fb759fb26036bed7d6cf2532526c7c3c6220c6
Nimble Capricorn <Nimble.capricorn@gmail.com>
2023-09-16 19:15:05 +0200

    generates a faulty program

--- #To create a program, fill what exercise.py
+++ #To create a program, fill what exercise.py
@@ -1,7 +1,13 @@
 from prettytable import PrettyTable
 #To create a program, fill what exercises you want to do, and what days of the week you want to work out on
-ExerciseList=[]
+ExerciseList=["santch", "clean and jerk","clean pull","squat","OHP"]
 Days=["hétfő","kedd", "csütörtök", "péntek"]
 #Create a table with the days and exercises
-Program=PrettyTable("Exercise", "Sets", "Reps", "PercentageOfOneRepMax")
-
+for Day in Days:
+    DailyTable = PrettyTable()
+    DailyTable.field_names=["Exercise", "Sets", "Reps", "PercentageOfOneRepMax"]
+    for Exercise in ExerciseList:
+        if ExerciseList.index(Exercise) <= (len(ExerciseList) / len(Days)):
+            DailyTable.add_row([Exercise, 3, 3, 70])
+    print(DailyTable)       
+            
