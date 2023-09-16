18f0d08bb498fc2c96ea7044621286080863d671
Nimble Capricorn <Nimble.capricorn@gmail.com>
2023-09-16 17:44:53 +0200

    created shitty program generator

---
+++ #To create a program, fill what exercise.py
@@ -0,0 +1,7 @@
+from prettytable import PrettyTable
+#To create a program, fill what exercises you want to do, and what days of the week you want to work out on
+ExerciseList=[]
+Days=["hétfő","kedd", "csütörtök", "péntek"]
+#Create a table with the days and exercises
+Program=PrettyTable("Exercise", "Sets", "Reps", "PercentageOfOneRepMax")
+
