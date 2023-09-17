9373fd07371d929bee1172258749d6a103f3ce32
Nimble Capricorn <Nimble.capricorn@gmail.com>
2023-09-17 14:35:00 +0200

    created non-slide_window chunking of exercises

--- #To create a program, fill what exercise.py
+++ #To create a program, fill what exercise.py
@@ -3,17 +3,26 @@ import math
 #To create a program, fill what exercises you want to do, and what days of the week you want to work out on
 ExerciseList=["santch", "clean and jerk","clean pull","squat","OHP"]
 Days=["hétfő","kedd", "csütörtök", "péntek"]
+bool_SlidingWindow=False #Set this if you would like to use sliding window instead of only doing each exercise once
 #functions and variables that make the generation easier
 def sliding_window(array, k):
     """give back k size subarrays of array using a sliding window"""
     for i in range(len(array)-k+1):
         yield array[i:i+k]
+def divide_chunks(l, n):
+    """Yield successive n-sized chunks from l.""" 
+    # looping till length l
+    for i in range(0, len(l), n): 
+        yield l[i:i + n]
 windowsize = math.ceil(len(ExerciseList)/len(Days))
-#Create a table with the days and exercises
+#Create a table with the days and exercises with sliding window
 for Day in Days:
     DailyTable = PrettyTable()
     DailyTable.field_names=["Exercise", "Sets", "Reps", "PercentageOfOneRepMax"]
-    for Exercise in tuple(sliding_window(ExerciseList, windowsize))[Days.index(Day)]:
-        DailyTable.add_row([Exercise, 3, 3, 70])        
-    print(DailyTable)       
-            
+    if(bool_SlidingWindow):
+        for Exercise in tuple(sliding_window(ExerciseList, windowsize))[Days.index(Day)]:
+            DailyTable.add_row([Exercise, 3, 3, 70])
+    else:
+        for Exercise in tuple(divide_chunks(ExerciseList, windowsize))[Days.index(Day)]:
+            DailyTable.add_row([Exercise, 3, 3, 70])        
+    print(DailyTable)               
