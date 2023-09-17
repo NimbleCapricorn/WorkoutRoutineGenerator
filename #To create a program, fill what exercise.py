10a36c542724c969c1642961d8613af975de2f25
Nimble Capricorn <Nimble.capricorn@gmail.com>
2023-09-17 12:07:52 +0200

    new script correctly geenrates one week of simple program

--- #To create a program, fill what exercise.py
+++ #To create a program, fill what exercise.py
@@ -1,13 +1,19 @@
 from prettytable import PrettyTable
+import math
 #To create a program, fill what exercises you want to do, and what days of the week you want to work out on
 ExerciseList=["santch", "clean and jerk","clean pull","squat","OHP"]
 Days=["hétfő","kedd", "csütörtök", "péntek"]
+#functions and variables that make the generation easier
+def window(array, k):
+    """give back k size subarrays of array"""
+    for i in range(len(array)-k+1):
+        yield array[i:i+k]
+windowsize = math.ceil(len(ExerciseList)/len(Days))
 #Create a table with the days and exercises
 for Day in Days:
     DailyTable = PrettyTable()
     DailyTable.field_names=["Exercise", "Sets", "Reps", "PercentageOfOneRepMax"]
-    for Exercise in ExerciseList:
-        if ExerciseList.index(Exercise) <= (len(ExerciseList) / len(Days)):
-            DailyTable.add_row([Exercise, 3, 3, 70])
+    for Exercise in tuple(window(ExerciseList, windowsize))[Days.index(Day)]:
+        DailyTable.add_row([Exercise, 3, 3, 70])        
     print(DailyTable)       
             
