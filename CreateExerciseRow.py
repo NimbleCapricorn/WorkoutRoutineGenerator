1dc98345dbe388fed575f40e44599ded2a6f5da1
Nimble Capricorn <Nimble.capricorn@gmail.com>
2023-09-19 12:33:39 +0200

    Intensity rounded to nearest tenth of a percent

--- CreateExerciseRow.py
+++ CreateExerciseRow.py
@@ -7,5 +7,5 @@ def createExerciseRow(name, VolumeSetting:Volume, IntensitySetting:Intensity):
     for IntensityIterator in IntensityList:
         if  IntensityIterator.name == IntensitySetting:
             IntermittentIntensity=IntensityIterator
-    Intensity=IntermittentIntensity.IntensityFunction(3) #how many reps you want to do
+    Intensity=round(IntermittentIntensity.IntensityFunction(3),1) #how many reps you want to do
     return Name, volume, 3, Intensity
