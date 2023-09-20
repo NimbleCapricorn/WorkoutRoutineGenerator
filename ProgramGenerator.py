5c67d1aedaed352decacc562be7149aa596b1578
Nimble Capricorn <Nimble.capricorn@gmail.com>
2023-09-20 07:18:55 +0200

    comment cleanup

--- ProgramGenerator.py
+++ ProgramGenerator.py
@@ -9,8 +9,8 @@ from ExerciseClass import *
 #To create a program, fill what exercises you want to do, and what days of the week you want to work out on. Weeks are volume-intensity pairs
 ProgramExerciseList=["snatch", "clean and jerk", "clean pull", "squat", "OHP", "push press", "squat"] ####TODO#### this list should be checked: does every name exist?
 Days=["Monday", "Tuesday", "Wednesday", "Friday"]
-Weeks=[Week(Volume.LOW,"LIGHT"), Week(Volume.MED,"LIGHTP")] #Not advised pairings are HIGH volume with MODP and up intensity, LOW volume with MED and down intensity (only for deloads)
-windowsize = math.ceil(len(ProgramExerciseList)/len(Days)) #Overwrite this to make other exercise groupings other than chunking up the ExerciseList into equal chunks
+Weeks=[Week(Volume.LOW,"LIGHT"), Week(Volume.MED,"LIGHTP")] #Not advised pairings are HIGH volume with MODP and up intensity (except for enhanced athletes), LOW volume with MOD and down intensity (only for deloads)
+windowsize = math.ceil(len(ProgramExerciseList)/len(Days)) #Overwrite this to make other exercise groupings other than chunking up the ExerciseList into equal parts
 bool_SlidingWindow=False #Set this if you would like to use sliding window instead of only doing each exercise once per mention in the list
 
 #Create a table with the days and exercises with the exerciselist subsets using either the sliding window or the chunks
