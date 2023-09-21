d5a5a6f525e75f24c98113276f4df2bc33da9b25
Nimble Capricorn <Nimble.capricorn@gmail.com>
2023-09-21 16:12:43 +0200

    sectioning fixes

--- WeekClass.py
+++ WeekClass.py
@@ -4,6 +4,7 @@ class Volume(Enum): ##TODO## Make it configurable, how many sets volume settings
     LOW=2
     MED=3
     HIGH=4
+
 class Intensity(Enum):
     LIGHT=0
     LIGHTP=1
@@ -12,6 +13,7 @@ class Intensity(Enum):
     HEAVY=4
     HEAVYP=5
     MAX=6
+
 class INOL_Target(Enum): #TODO# this should be configurable as well [but these are scientific limits, so they should not be too far off for anyone]
     Deload=0.4              #under 0.4 is too little for stimulating growth, but is perfect for deloading while still practicing skills
     DailyRecoverable=1.0    #between 0.4 and 1.0 is recoverable stimulus (next day you can repeat the same workout basically)
@@ -22,4 +24,3 @@ class Week:
     volume:Volume
     intensity:Intensity
     INOL_Target:INOL_Target
-
