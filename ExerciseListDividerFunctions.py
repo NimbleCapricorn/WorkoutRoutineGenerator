af9c00e374812362f34c75d02786c8a358248126
Nimble Capricorn <Nimble.capricorn@gmail.com>
2023-09-22 12:36:12 +0200

    deleted a comment

--- ExerciseListDividerFunctions.py
+++ ExerciseListDividerFunctions.py
@@ -7,6 +7,5 @@ def sliding_window(array, k):
 
 def divide_chunks(l, n):
     """Yield successive n-sized chunks from l.""" 
-    # looping till length l
     for i in range(0, len(l), n): 
         yield l[i:i + n]
