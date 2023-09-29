b9567c89841046bd993b6d64cc531f714c1db3fc
Nimble Capricorn <Nimble.capricorn@gmail.com>
2023-09-29 18:58:55 +0200

    bugfix in readin

--- Worklog_Readback_Analysis.py
+++ Worklog_Readback_Analysis.py
@@ -2,6 +2,5 @@
 from xlsxwriter import *
 from pandas import *
 from numpy import *
-WorkoutLog:DataFrame()
 with open('Output.xlsx','rb') as programfile:
     WorkoutLog=read_excel(programfile, 'WorkoutLog', header=["DateTime", "Exercise", "NumberOfSets", "NumberOfReps", "Weight", "OneRepMax", "RPE", "INOL"])
