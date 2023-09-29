03fa0f6831ec5c8df4164a6b92b7a977e493493c
Nimble Capricorn <Nimble.capricorn@gmail.com>
2023-09-29 19:29:07 +0200

    better separation in functions

--- Worklog_Readback_Analysis.py
+++ Worklog_Readback_Analysis.py
@@ -1,12 +1,15 @@
 #Worklog Readback and Analysis
 from xlsxwriter import *
+from Difficulty import *
 from pandas import *
-from numpy import *
 import os
 
 path=f"{os.getcwd()}/readback_analysis.xlsx"
 with open('Output.xlsx','rb') as programfile:
     WorkoutLog=read_excel(programfile, 'WorkoutLog') #header=["DateTime", "Exercise", "NumberOfSets", "NumberOfReps", "Weight", "OneRepMax", "RPE", "INOL"]
-    excel_writer=ExcelWriter(path, 'xlsxwriter')
-    WorkoutLog.to_excel(excel_writer)
-    excel_writer.close()
+    
+excel_writer=ExcelWriter(path, 'xlsxwriter')
+WorkoutLog.to_excel(excel_writer)
+
+
+excel_writer.close()
