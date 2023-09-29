f354f3fe67dcad0118d380ed9f459b38c72abd5d
Nimble Capricorn <Nimble.capricorn@gmail.com>
2023-09-29 19:14:20 +0200

    workout log readback works

--- Worklog_Readback_Analysis.py
+++ Worklog_Readback_Analysis.py
@@ -2,5 +2,11 @@
 from xlsxwriter import *
 from pandas import *
 from numpy import *
+import os
+
+path=f"{os.getcwd()}/readback_analysis.xlsx"
 with open('Output.xlsx','rb') as programfile:
-    WorkoutLog=read_excel(programfile, 'WorkoutLog', header=["DateTime", "Exercise", "NumberOfSets", "NumberOfReps", "Weight", "OneRepMax", "RPE", "INOL"])
+    WorkoutLog=read_excel(programfile, 'WorkoutLog') #header=["DateTime", "Exercise", "NumberOfSets", "NumberOfReps", "Weight", "OneRepMax", "RPE", "INOL"]
+    excel_writer=ExcelWriter(path, 'xlsxwriter')
+    WorkoutLog.to_excel(excel_writer)
+    excel_writer.close()
