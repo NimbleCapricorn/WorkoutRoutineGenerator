07a2493f87b71595fd99d3a8b4636f4a9213aecc
Nimble Capricorn <Nimble.capricorn@gmail.com>
2023-10-05 19:17:58 +0200

    Day Settings created but not yet used

---
+++ DayClass.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+import csv
+
+@dataclass
+class DayClass:
+    name:str
+    priority:float
+
+#Reading the users settings
+DaySettingList:DayClass=[]
+with open("Days.csv", "r") as ExercisesFile:
+    reader = csv.reader(ExercisesFile, delimiter=",")
+    
+    for row in reader:
+        newDay = DayClass(str(row[0]), float(row[1]))
+        DaySettingList.append(newDay)
