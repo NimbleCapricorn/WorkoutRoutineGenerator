ced52e67d38b260443400b3ffca28bdc08982a0c
Nimble Capricorn <Nimble.capricorn@gmail.com>
2023-10-14 19:48:35 +0200

    second "about" endpoint created

--- API/test.py
+++ API/test.py
@@ -5,3 +5,7 @@ app=FastAPI()
 @app.get("/")
 def home():
     return {"Data": "Test"}
+
+@app.get("/about")
+def about():
+    return {"Data": "This page shows general information about the webservice"}
