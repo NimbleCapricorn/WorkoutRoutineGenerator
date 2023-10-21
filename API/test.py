524368c275a64aa666e1afc0d7a1c43632f944f0
Nimble Capricorn <Nimble.capricorn@gmail.com>
2023-10-21 11:16:37 +0200

    API taken out of ProgramGenerator... again

--- API/test.py
+++ API/test.py
@@ -1,5 +1,23 @@
+import os
+from copy import *
+from tablib import *
+from pandas import *
+from subprocess import *
+from yaml import *
+from workoutroutinegenerator import WeekClass, DayClass, Warmup
+from workoutroutinegenerator.exerciseclass import ExerciseClass
 from fastapi import FastAPI
 
+
+##function definitions:##
+def findExercise(searching:ExerciseClass.Exercise):
+    for Exercise in ExerciseList:
+        if Exercise.Name==searching:
+            return Exercise
+##########################
+
+ExerciseList:ExerciseClass.Exercise=[]
+#API 
 app=FastAPI()
 
 @app.get("/")
@@ -9,3 +27,9 @@ def home():
 @app.get("/about")
 def about():
     return {"Data": "This page shows general information about the webservice"}
+
+@app.get("/get_exercise/{exercise}")
+def get_exercise(requested_exercise:str):
+    for exercise in ExerciseList:
+        if exercise.Name==requested_exercise:
+            return f'{exercise.Name}' #not found, because the ExerciseList is not populated in the API
--- ProgramGenerator.py
+++ ProgramGenerator.py
@@ -8,8 +8,6 @@ from subprocess import *
 from yaml import *
 from workoutroutinegenerator import WeekClass, DayClass, Warmup
 from workoutroutinegenerator.exerciseclass import ExerciseClass
-from fastapi import FastAPI
-
 
 ##function definitions:##
 def findExercise(searching:ExerciseClass.Exercise):
@@ -131,19 +129,3 @@ if generateWorkoutLog:
                                                     ]})
 Writer.close()
 ####################################################
-#API 
-app=FastAPI()
-
-@app.get("/")
-def home():
-    return {"Data": "Test"}
-
-@app.get("/about")
-def about():
-    return {"Data": "This page shows general information about the webservice"}
-
-@app.get("/get_exercise/{exercise}")
-def get_exercise(requested_exercise:str):
-    for exercise in ExerciseList:
-        if exercise.Name==requested_exercise:
-            return f'{exercise.Name}' #not found, because the ExerciseList is not populated in the API
