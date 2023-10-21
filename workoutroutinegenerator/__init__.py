9a77307997ad7dfffc6b68557cf33b667985e115
Nimble Capricorn <Nimble.capricorn@gmail.com>
2023-10-21 21:45:10 +0200

    __init__ now imports files

---
+++ workoutroutinegenerator/__init__.py
@@ -0,0 +1,3 @@
+from ProgramSettingWeek import ProgramSettingWeek, searchINOLSetting, searchIntensitySetting, searchVolumeSetting
+from DayClass import DayClass
+from Warmup import Warmup, GenerateWarmup
